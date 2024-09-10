ac8f98cbb5f27516dabb41249198f3293326f778
vladlenth <vladbass445@gmail.com>
2024-09-10 16:58:33 +0300

    Create CenturyFromYear.js

---
+++ loops/CenturyFromYear.js
@@ -0,0 +1,3 @@
+function century(year) {
+  return Math.ceil(year/100);  // The Math.ceil() static method always rounds up and returns the smallest integer greater than or equal to a given number.
+}
